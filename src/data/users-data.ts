64a5a4df7078284e815626bd77e894e6f6d1a820
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-07-02 19:46:46 +0300

    feat: add server and database data with users

---
+++ src/data/users-data.ts
@@ -0,0 +1,28 @@
+import { IUser } from "../types/types.js";
+
+export const usersList: Array<IUser> = [
+  {
+    id: '1',
+    username: 'Winnie the Pooh',
+    age: 18,
+    hobbies: ['eat honey', 'helping neighbors'],
+  },
+  {
+    id: '2',
+    username: 'Christopher Robin',
+    age: 10,
+    hobbies: ['adventures'],
+  },
+  {
+    id: '3',
+    username: 'Piglet',
+    age: 15,
+    hobbies: ['playing with Winnie', 'be a small animal'],
+  },
+  {
+    id: '4',
+    username: 'Rabbit',
+    age: 45,
+    hobbies: ['gardening'],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-import { test } from "./modules/test.js";
+import { IncomingMessage, Server, ServerResponse, createServer } from "http";
 
-const message: string = 'Hello Node!!';
-console.log(message);
+const PORT = process.env.PORT || 4000;
 
-test('test');
+const myServer: Server<typeof IncomingMessage, typeof ServerResponse> = createServer(async (req, res) => {
+  if (req.url?.startsWith("/api/users")) {
+    console.log('work with server and ', req.url);
+    res.end('Request accepted');
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+
+myServer.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`);
+});
+
